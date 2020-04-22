9b1d58e34753b6fecae098a1c510db1654c8503a
Andrea Delli <andrea.delli2001@gmail.com>
2020-04-22 15:23:17 +0200

    Exercise 10

---
+++ exercises/10/time-server.js
@@ -0,0 +1,15 @@
+const net = require("net"); // Network Node module
+const strftime = require("strftime"); // to format time easily
+
+const port = process.argv[2]; // Port where the server has to listen from
+
+net
+  // Create the new server
+  .createServer((socket) => {
+    // Write the current date into the socket
+    socket.write(strftime("%F %H:%M"));
+
+    // Close the socket (we have to write "\n" to the socket because that's how it is required from the exercise)
+    socket.end("\n");
+  })
+  .listen(port);
